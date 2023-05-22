17647742f8307a8a58d8ecc4f6d1dcdede436a47
startwarfields <j.warfieldmeyer@gmail.com>
2023-05-21 18:47:45 -0700

    added stuff

---
+++ src/pages/Sidebar.tsx
@@ -0,0 +1,28 @@
+import {BeakerIcon, BookOpenIcon, AcademicCapIcon} from "@heroicons/react/solid";
+import { Component, FunctionComponent } from "react";
+
+
+const SideBar = () => {
+  return (
+    <div className="fixed top-1/3 left-0 w-16 m-0 flex flex-col
+      bg-gradient-to-b from-orange-50 to-slate-200  text-orange-50 shadow-lg rounded-2xl">
+        <SideBarIcon icon={<BeakerIcon/>} />
+        <SideBarIcon icon={<BookOpenIcon/>} />
+        <SideBarIcon icon={<AcademicCapIcon/>} />
+    </div>
+  );
+};
+
+interface LayoutProps {
+  icon: JSX.Element;
+}
+
+const SideBarIcon: React.FC<LayoutProps>  = ({icon}) => (
+  <div className="sidebar-icon">
+  {icon}
+  </div>
+);
+
+export default SideBar;
+
+
