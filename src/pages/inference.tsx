adbd4192d74c5096ca4962baa35bb29632e275a5
startwarfields <j.warfieldmeyer@gmail.com>
2023-05-24 23:42:33 -0700

    society

--- src/pages/inference.tsx
+++ src/pages/inference.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 async function inference() {
   try {
-    env.wasm.wasmPaths = process.env.NODE_ENV === 'production' ? 'public/' : '/';
+    env.wasm.wasmPaths = process.env.NODE_ENV === 'production' ? 'static/wasm/' : '/';
     env.wasm.simd = true;
     const session = await InferenceSession.create('pipeline_xgboost.onnx');
 
