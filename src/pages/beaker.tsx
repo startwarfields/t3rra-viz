3d83c86a448905f7c9474380ab2163ad2964a227
startwarfields <j.warfieldmeyer@gmail.com>
2023-05-25 18:11:10 -0700

    tidyup

--- src/pages/beaker.tsx
+++ src/pages/beaker.tsx
@@ -3,39 +3,19 @@ import Example from "./chart";
 
 const BeakerPage: React.FC = () => {
   return (
-    <div className="text-white">
-      <h1>Beaker Projects</h1>
+    <div className="flex text-white justify-center">
+      <h1 className="text-slate-500">Flower Predictor</h1>
       
       <section>
-        <h2>Project 1</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          lacinia risus vitae elit vehicula, at sodales enim eleifend. Vivamus
-          non nulla et ante lacinia commodo. Nullam auctor tristique mauris, ac
-          dignissim ex.
+          Adjust the sliders below and click on the predict text to see how the machine learning model thinks which class
+          of flower it is 
         </p>
         <p>
-          <strong>Project Details:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Donec ultrices placerat eleifend. Nulla venenatis turpis non
-          sagittis eleifend.
-        </p>
+          </p>
       </section>
-      
-      <section className="project">
-        <h2>Project 2</h2>
-        <p>
-          Integer a fermentum velit, nec aliquam arcu. Curabitur feugiat
-          pharetra ante vitae feugiat. Fusce placerat efficitur ligula, at
-          efficitur est elementum at.
-        </p>
-        <p>
-          <strong>Project Details:</strong> Phasellus id malesuada nunc, ut tincidunt nisl.
-          Pellentesque malesuada eu mauris eget vulputate.
-        </p>
-      </section>
-      
+            
       <div className="flex justify-center items-center">
-        <Example />
       </div>
     </div>
   );
