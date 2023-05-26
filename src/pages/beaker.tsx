758df39f908e65acebe5089744320cfbebaea1c9
startwarfields <j.warfieldmeyer@gmail.com>
2023-05-25 18:20:52 -0700

    wording

--- src/pages/beaker.tsx
+++ src/pages/beaker.tsx
@@ -3,17 +3,25 @@ import Example from "./chart";
 
 const BeakerPage: React.FC = () => {
   return (
-    <div className="flex text-white justify-center">
+    <div className="text-white justify-center">
       <h1 className="text-slate-500">Flower Predictor</h1>
       
       <section>
         <p>
-          Adjust the sliders below and click on the predict text to see how the machine learning model thinks which class
-          of flower it is 
+         Adjust the sliders below and click predict to see what flower has the highest prediction score. 
+          This application inferences a xgboost model trained on the iris dataset. 
+          onnxruntime allows native typescript inference!         
         </p>
-        <p>
-          </p>
+       
       </section>
+
+ 
+      <section>
+             <p>This website is a test bench for me learning web development </p>
+     
+      </section>
+
+      
             
       <div className="flex justify-center items-center">
       </div>
